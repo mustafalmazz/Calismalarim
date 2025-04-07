461e16fc39635fcf919601487d151d7fd8c5d226
mustafalmazz <116174558+mustafalmazz@users.noreply.github.com>
2025-04-07 20:52:43 +0300

    GuvenliKodYazma

---
+++ VizeKapsulleme/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kapsülleme2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Emlak
+    {
+        public string semt;
+        private string renk;
+        private int odasayisi;
+        private int katno;
+        private double alan;
+
+        public string SEMT
+        {
+            get { return semt; }
+            set { semt = value; }
+        }
+
+        public string RENK
+        {
+            get { return renk; }
+            set { renk = value; }
+        }
+
+    }
+}
