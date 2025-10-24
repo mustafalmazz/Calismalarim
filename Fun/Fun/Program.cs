7ceddf64c1efa610674079213c8d0facd0c7783b
Mustafa ALMAZ <116174558+mustafalmazz@users.noreply.github.com>
2025-10-24 18:59:12 +0300

    Add files via upload

---
+++ Fun/Fun/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Fun
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int x = 0;
+            while (x>20)
+            {
+                Console.WriteLine("Problemler Aranıyor !!");
+                Thread.Sleep(1);
+                Console.WriteLine("Bir sorun bulamadık !!");
+                x = x + 1;
+                
+            }
+            
+        }
+    }
+}
